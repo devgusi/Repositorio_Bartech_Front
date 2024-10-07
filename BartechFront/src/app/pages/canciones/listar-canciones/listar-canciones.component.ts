215180a95b123904ba77697fc76a90020d9689eb
Oscar_jaimes_gomez <oscarjaimes904@gmail.com>
2024-10-07 10:50:48 -0500

    actualizacion codigo

--- BartechFront/src/app/pages/canciones/listar-canciones/listar-canciones.component.ts
+++ BartechFront/src/app/pages/canciones/listar-canciones/listar-canciones.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as QRCode from 'qrcode'; // Importar la librería QRCode
 
 @Component({
   selector: 'app-listar-canciones',
@@ -7,13 +7,24 @@ import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./listar-canciones.component.scss']
 })
 export class ListarCancionesComponent implements OnInit {
-
-  qrData: string = 'http://www.youtube.com'; // URL o texto que deseas codificar
+  // qrData: string = 'http://localhost:4200/registrarCancion';
+  qrData: string = 'https://www.youtube.com/watch?v=wunUvgf-IG4';
+  
 
   constructor() { }
 
   ngOnInit(): void {
-  
+    this.generateQRCode();
   }
 
+  generateQRCode(): void {
+    const canvas = document.getElementById('qrcode') as HTMLCanvasElement;
+    QRCode.toCanvas(canvas, this.qrData, function (error: any) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log('QR code generated successfully!');
+      }
+    });
+  }
 }
