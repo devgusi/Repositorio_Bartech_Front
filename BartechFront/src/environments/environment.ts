76abda08e5a12250ae89933357affa6be8c6a279
gustavo delgado <gdelgadog1@ucentral.edu.co>
2024-10-29 16:15:27 -0500

    Cambio de url del Back

--- BartechFront/src/environments/environment.ts
+++ BartechFront/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   entorno: 'prod',
   //SERVICES PLANEACIÓN
-  CRUD_BARTECH: 'http://172.19.3.44:8084/',
+  CRUD_BARTECH: 'http://172.19.3.253:8084/',
 
   //CONSTANTE DEL QR
   qrData : 'http://localhost:4200/registrarCancion',
