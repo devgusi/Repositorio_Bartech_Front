400dc528aed426ad60734d4ae28641719f3354bd
gustavo delgado <gdelgadog1@ucentral.edu.co>
2024-11-22 16:14:46 -0500

    Apuntamiento del crud

--- BartechFront/src/environments/environment.prod.ts
+++ BartechFront/src/environments/environment.prod.ts
@@ -2,9 +2,10 @@ export const environment = {
   production: true,
   entorno: 'prod',
   //SERVICES PLANEACIÓN
-  CRUD_BARTECH: 'https://141.148.73.46/',
+  //CRUD_BARTECH: 'https://141.148.73.46/',
   //CRUD_BARTECH: 'http://localhost:8080/',
-  //CRUD_BARTECH: 'http://configserver.icfes.edu.co/'
+  //CRUD_BARTECH: 'https://configserver.icfes.edu.co/',
+  CRUD_BARTECH: 'http://192.168.1.16:8080/',
 
   //CONSTANTE DEL QR
   //const qrData = 'http://localhost:4200/registrarCancion';
--- BartechFront/src/environments/environment.ts
+++ BartechFront/src/environments/environment.ts
@@ -3,8 +3,9 @@ export const environment = {
   entorno: 'prod',
   //SERVICES PLANEACIÓN
   //CRUD_BARTECH: 'http://localhost:8080/',
-  CRUD_BARTECH: 'https://141.148.73.46/',
-  //CRUD_BARTECH: 'http://configserver.icfes.edu.co/'
+  //CRUD_BARTECH: 'https://141.148.73.46/',
+  //CRUD_BARTECH: 'https://configserver.icfes.edu.co/',
+  CRUD_BARTECH: 'http://192.168.1.16:8080/',
 
   //CONSTANTE DEL QR
   qrData : 'https://k24fpfim43xwqjftf4fza477vu.apigateway.us-ashburn-1.oci.customer-oci.com/registrarCancion',
